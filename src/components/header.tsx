08edeb60d120bb826f5b0de8d66bd2d6d8c05c26
Zaid Hernandez <zaidmiguelhernandez@gmail.com>
2025-09-14 21:31:52 +0000

    react-dom-client.development.js:3860 Uncaught Error: Maximum update dept

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,13 @@
 
-'use server';
+'use client'
 
-import { createClient } from '@/lib/supabase/server';
 import { MainNav } from './main-nav';
 import { UserNav } from './user-nav';
 import { MobileNav } from './mobile-nav';
 import { type User } from '@supabase/supabase-js';
 import { type Profile } from '@/lib/types';
 
-export async function Header({user, profile}: {user?:User | null, profile?: Profile | null}) {
-  if(!user){
-    const supabase = await createClient();
-    const {
-      data: { user: supabaseUser },
-    } = await supabase.auth.getUser();
-    user = supabaseUser
-  }
-
-
+export function Header({user, profile}: {user?:User | null, profile?: Profile | null}) {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
